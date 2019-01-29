d6c528eb310ae193d054d780cb8df5faac684e93
kidsense2019 <kidsense2019@gmail.com>
2019-01-29 12:41:07 +0800

    first

---
+++ app/src/main/java/com/example/kidsense2019/Session.java
@@ -0,0 +1,96 @@
+package com.example.kidsense2019;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by ASUS on 12/02/2017.
+ */
+public class Session {
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+    private Context ctx;
+    private String username, password, email, status, token;
+    private String ip = "203.189.120.153";
+
+    public Session(Context ctx) {
+        this.ctx = ctx;
+        prefs = ctx.getSharedPreferences("myApp", Context.MODE_PRIVATE);
+        editor = prefs.edit();
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        editor.putBoolean("loggedInmode",loggedIn);
+        editor.commit();
+    }
+
+    public void saveIP() {
+        editor.putString("ip", ip);
+        editor.commit();
+    }
+
+    public void saveFCM(String token) {
+        this.token = token;
+        editor.putString("token", token);
+        editor.commit();
+    }
+
+
+    public void saveUsername(String username) {
+        this.username = username;
+        editor.putString("username", username);
+        editor.commit();
+    }
+
+    public void saveEmail(String email) {
+        this.email = email;
+        editor.putString("email", email);
+        editor.commit();
+    }
+
+    public void savePassword(String password) {
+        this.password = password;
+        editor.putString("password", password);
+        editor.commit();
+    }
+
+    public void saveStatus(String status) {
+        this.status = status;
+        editor.putString("status", status);
+        editor.commit();
+    }
+
+    public String getsaveUsername() {
+        return prefs.getString("username", username);
+    }
+
+    public String getfullName() {
+
+        return prefs.getString("token", token);
+    }
+
+    public String getsaveEmail() {
+
+        return prefs.getString("email", email);
+    }
+
+    public String getsavePassword() {
+
+        return prefs.getString("password", password);
+    }
+
+    public String getsaveStatus() {
+
+        return prefs.getString("status", status);
+    }
+
+    public String getIP() {
+
+        return prefs.getString("ip", ip);
+    }
+
+    public boolean loggedin() {
+
+        return prefs.getBoolean("loggedInmode",false);
+    }
+}
---
+++ app/src/main/java/com/example/kidsense2019/fcmObjects/myFirebaseMessagingService.java
@@ -0,0 +1,128 @@
+package com.example.kidsense2019.fcmObjects;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.kidsense2019.MainActivity;
+import com.example.kidsense2019.Session;
+import com.example.kidsense2019.location.MapsActivity;
+import com.example.kidsense2019.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class myFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        System.out.println("New Fb Token : " + s);
+        //Log.d("token", "Refreshed token: " + s);
+        Session session = new Session(getApplicationContext());
+        session.saveFCM(s);
+        System.out.println("save complete");
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getData() != null) {
+            sendNotification(remoteMessage);
+        }
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+
+        int uniqueId = 0;
+        Intent intent = null;
+        String NOTIFICATION_CHANNEL_ID = "kidsense", contentText = null, contentTitle = null;
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            @SuppressLint("WrongConstant")
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    "kidsense Notification", NotificationManager.IMPORTANCE_MAX);
+
+            notificationChannel.setDescription("Kidsense Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableVibration(true);
+
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        Map<String, String> data = remoteMessage.getData();
+        String content = data.get("content");
+        switch (content) {
+            case "Location" :
+                // set Unique Id
+                uniqueId = (int) (System.currentTimeMillis() & 0xfffffff);
+                // intent
+                intent = new Intent(this, MapsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("content", content);
+                intent.putExtra("name", data.get("name"));
+                intent.putExtra("latitude", data.get("latitude"));
+                intent.putExtra("longitude", data.get("longitude"));
+                intent.putExtra("timestamp", data.get("timestamp"));
+                // inbox style
+                inboxStyle.setBigContentTitle(data.get("name"));
+                inboxStyle.addLine("Lat : " + data.get("latitude"));
+                inboxStyle.addLine("Lng : " + data.get("longitude"));
+                inboxStyle.addLine("When : " + data.get("timestamp"));
+                inboxStyle.setSummaryText("Kidsense");
+                // notif builder
+                contentTitle = data.get("name");
+                contentText = data.get("timestamp");
+                break;
+            case "Kid Account Auth" :
+                // set Unique Id
+                uniqueId = 1;
+                // intent
+                intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                // inbox style
+                inboxStyle.setBigContentTitle(data.get("name"));
+                inboxStyle.addLine("Code : " + data.get("code"));
+                inboxStyle.setSummaryText("Kidsense");
+                // notif builder
+                contentTitle = data.get("name");
+                contentText = data.get("code");
+                break;
+            default:
+                break;
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, uniqueId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_kidsense_logo)
+                .setTicker("New Incoming " + content)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setStyle(inboxStyle)
+                .setContentInfo(content)
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(uniqueId,notificationBuilder.build());
+    }
+}
